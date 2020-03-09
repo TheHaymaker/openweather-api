78b861d00cdfc29465b96ab7f7e4483d77553cd1
Brett Haymaker <brett.haymaker@gmail.com>
2020-03-08 22:51:14 -0500

    Updates

---
+++ precache-manifest.ab6b9533fdc843949352536b56577ca4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21d26ada67b73ea03cca09855bf8aded",
+    "url": "/openweather-api/index.html"
+  },
+  {
+    "revision": "9ec63920d6fb0008f385",
+    "url": "/openweather-api/static/css/main.c8b81b72.chunk.css"
+  },
+  {
+    "revision": "0809d55e195fc3ba6a45",
+    "url": "/openweather-api/static/js/2.47900e66.chunk.js"
+  },
+  {
+    "revision": "28e464ea91017ce17d57fbc4d999633f",
+    "url": "/openweather-api/static/js/2.47900e66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ec63920d6fb0008f385",
+    "url": "/openweather-api/static/js/main.e0c0f045.chunk.js"
+  },
+  {
+    "revision": "60601ec088b5b464fa02",
+    "url": "/openweather-api/static/js/runtime-main.39f43434.js"
+  },
+  {
+    "revision": "915db55868bfb0ff3675fbbcd8e4e4e5",
+    "url": "/openweather-api/static/media/degree.915db558.svg"
+  }
+]);
